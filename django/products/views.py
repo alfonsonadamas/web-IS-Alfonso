ac6928ecaee133453d5010ff2239ce086af6b52a
alfonsonadamas <gallardoalfonso01@gmail.com>
2022-09-06 16:55:26 -0500

    Django inicial

---
+++ django/products/views.py
@@ -0,0 +1,10 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpRequest
+
+# Create your views here.
+def index(request):
+    
+    return HTTPResponse("Hola mundo")
+
+
